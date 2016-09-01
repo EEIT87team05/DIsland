7806fe39e05cb3f81bc87556627a6a42773d625a
陳奕廷 <xxxooo@gmail.com>
2016-09-01 11:49:35 +0800

    11.49

--- disland/src/main/java/com/disland/www/disland/qqqq.java
+++ disland/src/main/java/com/disland/www/disland/qqqq.java
@@ -9,6 +9,7 @@ public class qqqq {
 		System.out.println("GGGG");
 		System.out.println("123456");
 		System.out.println("chichi TEST 20160901 11:45");
+		System.out.println(" TEST 20160901 11:49");
 	}
 
 }
