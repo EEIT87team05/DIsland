2d75f91b220b539c3f66f6b989e10c0abc0c8045
詹惟仁 <Student@EEIT-PC>
2016-09-01 12:01:26 +0800

    22

--- disland/src/main/java/com/disland/www/disland/qqqq.java
+++ disland/src/main/java/com/disland/www/disland/qqqq.java
@@ -14,7 +14,9 @@ public class qqqq {
 		System.out.println("123456");
 		System.out.println("chichi TEST 20160901 11:45");
 		System.out.println(" TEST 20160901 11:49");
-
+		
+		
+		System.out.println("REN TEST 20160901 11:59");
 	}
 
 }
